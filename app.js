a54816039c1408cb2c2993afce691e533f188e23
Jessi Velazquez <jessivelazq1@gmail.com>
2021-02-11 16:01:36 -0800

    Updated JS file with functions

--- app.js
+++ app.js
@@ -3,151 +3,167 @@
 let userName = prompt("What is your name?")
 alert ("Welcome to my 'About Me' site, " + userName +".")
 let correctCount = 0
-let gradYearUW = prompt("Did I graduate from University of Washington in 2008?");
-let lowergradYearUW = gradYearUW.toLowerCase();
 
-
-switch(lowergradYearUW) {
-    case "yes":
-        //console.log("Correct, I did graduate UW in 2008!");
-        alert("Correct, I did graduate UW in 2008!");
-        correctCount++
-        break;
-    case "y":
-        //console.log("Correct, I did graduate UW in 2008!");
-        alert("Correct, I did graduate UW in 2008!");
-        correctCount++
-        break;
-    case "true":
-        //console.log("Correct, I did graduate UW in 2008!");
-        alert("Correct, I did graduate UW in 2008!");
-        correctCount++
-        break;
-    case "t":
-        //console.log("Correct, I did graduate UW in 2008!");
-        alert("Correct, I did graduate UW in 2008!");
-        correctCount++
-        break;
-    default:
-        //console.log("Incorrect. I graduated UW in 2008.");
-        alert("Incorrect. I graduated UW in 2008.");
+function gradQuestion() {
+    let gradYearUW = prompt("Did I graduate from University of Washington in 2008?");
+    let lowergradYearUW = gradYearUW.toLowerCase();
+    switch(lowergradYearUW) {
+        case "yes":
+            //console.log("Correct, I did graduate UW in 2008!");
+            alert("Correct, I did graduate UW in 2008!");
+            correctCount++
+            break;
+        case "y":
+            //console.log("Correct, I did graduate UW in 2008!");
+            alert("Correct, I did graduate UW in 2008!");
+            correctCount++
+            break;
+        case "true":
+            //console.log("Correct, I did graduate UW in 2008!");
+            alert("Correct, I did graduate UW in 2008!");
+            correctCount++
+            break;
+        case "t":
+            //console.log("Correct, I did graduate UW in 2008!");
+            alert("Correct, I did graduate UW in 2008!");
+            correctCount++
+            break;
+        default:
+            //console.log("Incorrect. I graduated UW in 2008.");
+            alert("Incorrect. I graduated UW in 2008.");
+    }
 }
 
-let hobbyHike = prompt("Do I like to hike?");
-let lowerhobbyHike = hobbyHike.toLowerCase();
+gradQuestion();
 
-switch(lowerhobbyHike) {
-    case "yes":
-        //console.log("Correct, I do like to hike!");
-        alert("Correct, I do like to hike!");
-        correctCount++
-        break;
-    case "y":
-        //console.log("Correct, I do like to hike!");
-        alert("Correct, I do like to hike!");
-        correctCount++
-        break;
-    case "true":
-        //console.log("Correct, I do like to hike!");
-        alert("Correct, I do like to hike!");
-        correctCount++
-        break;
-    case "t":
-        //console.log("Correct, I do like to hike!");
-        alert("Correct, I do like to hike!");
-        correctCount++
-        break;
-    default:
-        //console.log("Incorrect. I like to hike.");
-        alert("Incorrect. I like to hike.");
+function hikeQuestion() {
+    let hobbyHike = prompt("Do I like to hike?");
+    let lowerhobbyHike = hobbyHike.toLowerCase();
+    switch(lowerhobbyHike) {
+        case "yes":
+            //console.log("Correct, I do like to hike!");
+            alert("Correct, I do like to hike!");
+            correctCount++
+            break;
+        case "y":
+            //console.log("Correct, I do like to hike!");
+            alert("Correct, I do like to hike!");
+            correctCount++
+            break;
+        case "true":
+            //console.log("Correct, I do like to hike!");
+            alert("Correct, I do like to hike!");
+            correctCount++
+            break;
+        case "t":
+            //console.log("Correct, I do like to hike!");
+            alert("Correct, I do like to hike!");
+            correctCount++
+            break;
+        default:
+            //console.log("Incorrect. I like to hike.");
+            alert("Incorrect. I like to hike.");
+    }
 }
 
-let jobInnovasea = prompt("Did I work for Innovasea?");
-let lowerjobInnovasea = jobInnovasea.toLowerCase();
+hikeQuestion();
 
-switch(lowerjobInnovasea) {
-    case "yes":
-        //console.log("Correct! Yes I did");
-        alert("Correct! Yes I did");
-        correctCount++
-        break;
-    case "y":
-        //console.log("Correct! Yes I did");
-        alert("Correct! Yes I did");
-        correctCount++
-        break;
-    case "true":
-        //console.log("Correct! Yes I did");
-        alert("Correct! Yes I did");
-        correctCount++
-        break;
-    case "t":
-        //console.log("Correct! Yes I did");
-        alert("Correct! Yes I did");
-        correctCount++
-        break;
-    default:
-        //console.log("Incorrect, I did work for Innovasea.");
-        alert("Incorrect, I did work for Innovasea.");
+function workQuestion() {
+    let jobInnovasea = prompt("Did I work for Innovasea?");
+    let lowerjobInnovasea = jobInnovasea.toLowerCase();
+    switch(lowerjobInnovasea) {
+        case "yes":
+            //console.log("Correct! Yes I did");
+            alert("Correct! Yes I did");
+            correctCount++
+            break;
+        case "y":
+            //console.log("Correct! Yes I did");
+            alert("Correct! Yes I did");
+            correctCount++
+            break;
+        case "true":
+            //console.log("Correct! Yes I did");
+            alert("Correct! Yes I did");
+            correctCount++
+            break;
+        case "t":
+            //console.log("Correct! Yes I did");
+            alert("Correct! Yes I did");
+            correctCount++
+            break;
+        default:
+            //console.log("Incorrect, I did work for Innovasea.");
+            alert("Incorrect, I did work for Innovasea.");
+    }
 }
 
-let soloAlbum = prompt("Have I ever released a solo album?");
-let lowersoloAlbum = soloAlbum.toLowerCase();
+workQuestion();
 
-switch(lowersoloAlbum) {
-    case "yes":
-        //console.log("Correct, I have released a solo album.");
-        alert("Correct, I have released a solo album.");
-        correctCount++
-        break;
-    case "y":
-        //console.log("Correct, I have released a solo album.");
-        alert("Correct, I have released a solo album.");
-        correctCount++
-        break;
-    case "true":
-        //console.log("Correct, I have released a solo album.");
-        alert("Correct, I have released a solo album.");
-        correctCount++
-        break;
-    case "t":
-        //console.log("Correct, I have released a solo album.");
-        alert("Correct, I have released a solo album.");
-        correctCount++
-        break;
-    default:
-        //console.log("Incorrect. I have released multiple of them.");
-        alert("Incorrect, I have released multiple of them.");
+function soloAlbumQuestion() {
+    let soloAlbum = prompt("Have I ever released a solo album?");
+    let lowersoloAlbum = soloAlbum.toLowerCase();
+    switch(lowersoloAlbum) {
+        case "yes":
+            //console.log("Correct, I have released a solo album.");
+            alert("Correct, I have released a solo album.");
+            correctCount++
+            break;
+        case "y":
+            //console.log("Correct, I have released a solo album.");
+            alert("Correct, I have released a solo album.");
+            correctCount++
+            break;
+        case "true":
+            //console.log("Correct, I have released a solo album.");
+            alert("Correct, I have released a solo album.");
+            correctCount++
+            break;
+        case "t":
+            //console.log("Correct, I have released a solo album.");
+            alert("Correct, I have released a solo album.");
+            correctCount++
+            break;
+        default:
+            //console.log("Incorrect. I have released multiple of them.");
+            alert("Incorrect, I have released multiple of them.");
+    }
 }
 
-let whidbeyIsland = prompt("Do I live on Whidbey Island?");
-let lowerwhidbeyIsland = whidbeyIsland.toLowerCase();
+soloAlbumQuestion();
 
-switch(lowerwhidbeyIsland) {
-    case "yes":
-        //console.log("Correct, I live on Whidbey Island.");
-        alert("Correct, I live on Whidbey Island.");
-        correctCount++
-        break;
-    case "y":
-        //console.log("Correct, I live on Whidbey Island.");
-        alert("Correct, I live on Whidbey Island.");
-        correctCount++
-        break;
-    case "true":
-        //console.log("Correct, I live on Whidbey Island.");
-        alert("Correct, I live on Whidbey Island.");
-        correctCount++
-        break;
-    case "t":
-        //console.log("Correct, I live on Whidbey Island.");
-        alert("Correct, I live on Whidbey Island.");
-        correctCount++
-        break;
-    default:
-        //console.log("Incorrect, I do live on Whidbey Island.")
-        alert("Incorrect, I do live on Whidbey Island.")
+function whidbeyIslandQuestion() {
+    let whidbeyIsland = prompt("Do I live on Whidbey Island?");
+    let lowerwhidbeyIsland = whidbeyIsland.toLowerCase();
+    
+    switch(lowerwhidbeyIsland) {
+        case "yes":
+            //console.log("Correct, I live on Whidbey Island.");
+            alert("Correct, I live on Whidbey Island.");
+            correctCount++
+            break;
+        case "y":
+            //console.log("Correct, I live on Whidbey Island.");
+            alert("Correct, I live on Whidbey Island.");
+            correctCount++
+            break;
+        case "true":
+            //console.log("Correct, I live on Whidbey Island.");
+            alert("Correct, I live on Whidbey Island.");
+            correctCount++
+            break;
+        case "t":
+            //console.log("Correct, I live on Whidbey Island.");
+            alert("Correct, I live on Whidbey Island.");
+            correctCount++
+            break;
+        default:
+            //console.log("Incorrect, I do live on Whidbey Island.")
+            alert("Incorrect, I do live on Whidbey Island.")
+    }    
 }
+
+soloAlbumQuestion();
 
 function numberGuess() {
     let i = Math.floor(Math.random() * 100);
@@ -179,26 +195,26 @@ function numberGuess() {
 
 numberGuess();
 
-
-let favColors;
-favColors = ['blue', 'green', "yellow", "peach"]
-console.log('favorite colors', favColors)
-let guessAmount = 6
-
-for (let i = 1; i < guessAmount; i +=1) {
-    let colorGuess = prompt("Guess one of my favorite colors:")
-    if (colorGuess === favColors[0] || colorGuess === favColors[1] || colorGuess === favColors[2] || colorGuess === favColors[3]) {
-        console.log(colorGuess)
-        alert("You are correct!")
-        alert(favColors + " were possible answers")
-        correctCount++
-        break;
+function favColorGuess() {
+    let favColors;
+    favColors = ['blue', 'green', "yellow", "peach"]
+    console.log('favorite colors', favColors)
+    let guessAmount = 6
+    
+    for (let i = 1; i < guessAmount; i +=1) {
+        let colorGuess = prompt("Guess one of my favorite colors:")
+        if (colorGuess === favColors[0] || colorGuess === favColors[1] || colorGuess === favColors[2] || colorGuess === favColors[3]) {
+            console.log(colorGuess)
+            alert("You are correct!")
+            alert(favColors + " were possible answers")
+            correctCount++
+            break;
+        }
+        else {
+            alert("Incorrect, guess again:");
+        } 
     }
-    else {
-        alert("Incorrect, guess again:");
-    } 
 }
-
 
 alert("Thanks for visiting my site, " + userName + "! Bye for now. You got " + correctCount + " answers right.")
 
