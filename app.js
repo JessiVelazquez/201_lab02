460033da534e0a7205995e62ebecac910d8eed9e
Jessi Velazquez <jessivelazq1@gmail.com>
2021-02-12 17:05:12 -0800

    removed commented out console logs from app.js

--- app.js
+++ app.js
@@ -9,27 +9,22 @@ function gradQuestion() {
     let lowergradYearUW = gradYearUW.toLowerCase();
     switch(lowergradYearUW) {
         case "yes":
-            //console.log("Correct, I did graduate UW in 2008!");
             alert("Correct, I did graduate UW in 2008!");
             correctCount++
             break;
         case "y":
-            //console.log("Correct, I did graduate UW in 2008!");
             alert("Correct, I did graduate UW in 2008!");
             correctCount++
             break;
         case "true":
-            //console.log("Correct, I did graduate UW in 2008!");
             alert("Correct, I did graduate UW in 2008!");
             correctCount++
             break;
         case "t":
-            //console.log("Correct, I did graduate UW in 2008!");
             alert("Correct, I did graduate UW in 2008!");
             correctCount++
             break;
         default:
-            //console.log("Incorrect. I graduated UW in 2008.");
             alert("Incorrect. I graduated UW in 2008.");
     }
 }
@@ -41,27 +36,22 @@ function hikeQuestion() {
     let lowerhobbyHike = hobbyHike.toLowerCase();
     switch(lowerhobbyHike) {
         case "yes":
-            //console.log("Correct, I do like to hike!");
             alert("Correct, I do like to hike!");
             correctCount++
             break;
         case "y":
-            //console.log("Correct, I do like to hike!");
             alert("Correct, I do like to hike!");
             correctCount++
             break;
         case "true":
-            //console.log("Correct, I do like to hike!");
             alert("Correct, I do like to hike!");
             correctCount++
             break;
         case "t":
-            //console.log("Correct, I do like to hike!");
             alert("Correct, I do like to hike!");
             correctCount++
             break;
         default:
-            //console.log("Incorrect. I like to hike.");
             alert("Incorrect. I like to hike.");
     }
 }
@@ -73,27 +63,22 @@ function workQuestion() {
     let lowerjobInnovasea = jobInnovasea.toLowerCase();
     switch(lowerjobInnovasea) {
         case "yes":
-            //console.log("Correct! Yes I did");
             alert("Correct! Yes I did");
             correctCount++
             break;
         case "y":
-            //console.log("Correct! Yes I did");
             alert("Correct! Yes I did");
             correctCount++
             break;
         case "true":
-            //console.log("Correct! Yes I did");
             alert("Correct! Yes I did");
             correctCount++
             break;
         case "t":
-            //console.log("Correct! Yes I did");
             alert("Correct! Yes I did");
             correctCount++
             break;
         default:
-            //console.log("Incorrect, I did work for Innovasea.");
             alert("Incorrect, I did work for Innovasea.");
     }
 }
@@ -105,27 +90,22 @@ function soloAlbumQuestion() {
     let lowersoloAlbum = soloAlbum.toLowerCase();
     switch(lowersoloAlbum) {
         case "yes":
-            //console.log("Correct, I have released a solo album.");
             alert("Correct, I have released a solo album.");
             correctCount++
             break;
         case "y":
-            //console.log("Correct, I have released a solo album.");
             alert("Correct, I have released a solo album.");
             correctCount++
             break;
         case "true":
-            //console.log("Correct, I have released a solo album.");
             alert("Correct, I have released a solo album.");
             correctCount++
             break;
         case "t":
-            //console.log("Correct, I have released a solo album.");
             alert("Correct, I have released a solo album.");
             correctCount++
             break;
         default:
-            //console.log("Incorrect. I have released multiple of them.");
             alert("Incorrect, I have released multiple of them.");
     }
 }
@@ -138,32 +118,27 @@ function whidbeyIslandQuestion() {
     
     switch(lowerwhidbeyIsland) {
         case "yes":
-            //console.log("Correct, I live on Whidbey Island.");
             alert("Correct, I live on Whidbey Island.");
             correctCount++
             break;
         case "y":
-            //console.log("Correct, I live on Whidbey Island.");
             alert("Correct, I live on Whidbey Island.");
             correctCount++
             break;
         case "true":
-            //console.log("Correct, I live on Whidbey Island.");
             alert("Correct, I live on Whidbey Island.");
             correctCount++
             break;
         case "t":
-            //console.log("Correct, I live on Whidbey Island.");
             alert("Correct, I live on Whidbey Island.");
             correctCount++
             break;
         default:
-            //console.log("Incorrect, I do live on Whidbey Island.")
             alert("Incorrect, I do live on Whidbey Island.")
     }    
 }
 
-soloAlbumQuestion();
+whidbeyIslandQuestion();
 
 function numberGuess() {
     let i = Math.floor(Math.random() * 100);
@@ -215,6 +190,8 @@ function favColorGuess() {
         } 
     }
 }
+
+favColorGuess()
 
 alert("Thanks for visiting my site, " + userName + "! Bye for now. You got " + correctCount + " answers right.")
 
